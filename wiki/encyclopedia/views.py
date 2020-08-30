21c9a54408bc67da94696e01bab32cfb5653782a
golfyvicky <vivek.army@gmail.com>
2020-08-30 21:50:16 +0530

    Search function working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,24 +1,48 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
+    if request.POST :
+        
+        #searchtag = request.POST.get("q")
+        searchtagupper = request.POST.get("q").upper()
+        
+        entries = util.list_entries()
+        entriesupper = [entry.upper() for entry in entries]
+        
+        if searchtagupper in entriesupper:
+            return render(request, "encyclopedia/searchresult.html",{
+                "pagetitle" : searchtagupper,
+                "page": util.get_entry(request.POST.get("q"))
+            })
+        else:
+            return render(request, "encyclopedia/error.html",{
+                "searchtag" : request.POST.get("q")
+            })
+
+    else:
+        return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def checkentry(request, name):
-    #name1=util.get_entry(name)
-    if (util.get_entry(name)==None):
+    if (util.get_entry(name) == None):
         return render(request, "encyclopedia/error.html",{
             "nametitle":name
         })
-    else:    
-        return render(request, "encyclopedia/test.html",{
-            "nametitle" : name,
-            "name": util.get_entry(name)
-        })
+    else:
+        return render(request, "encyclopedia/searchresult.html",{
+            "pagetitle" : name,
+            "page": util.get_entry(name)
+    })
         
-    
+def search(request,searchtag):
+    entries = util.list_entries()
+    search_box = request.POST.get("q").capitalize()
+    #search_box = searchtag.capitalize()
+
+    if search_box in entries:
+        return HttpResponseRedirect(f"wiki/{search_box}")
 
